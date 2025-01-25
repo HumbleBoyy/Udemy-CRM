b74a8823b28d87be62aa791c355a1c5a0eb07e62
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-25 18:41:19 +0500

    new Commit

--- src/Components/InfoHome/InfoHome.jsx
+++ src/Components/InfoHome/InfoHome.jsx
@@ -5,8 +5,8 @@ const InfoHome = ({icon,text,text2}) => {
     <div className='flex gap-5'>
        <div className='bg-[#EFF3FA] w-[40px] h-[40px] p-2 text-[30px] flex items-center text-[#13296A] rounded-md'>{icon}</div>
        <div className='flex flex-col gap-5'>
-          <h2 className='text-[25px] text-[#4F4F4F]'>{text}</h2>
-          <p className='text-15px w-[]'>Create rich course content and coaching products for your students. <br /> When you give them a pricing plan, they’ll appear on your site!</p>
+          <h2 className='text-[25px] text-[#4F4F4F] font-semibold'>{text}</h2>
+          <p className='text-15px'>Create rich course content and coaching products for your students. <br /> When you give them a pricing plan, they’ll appear on your site!</p>
        </div>
     </div>
   )
