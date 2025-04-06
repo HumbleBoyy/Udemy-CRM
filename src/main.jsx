7f027062833ab214c6f04e7ccf4998318e132d93
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 00:05:38 +0500

    New Commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { TokenContext } from './Context/Context.jsx'
-
+import Logo from "./assets/Logos/MainLogo.svg"
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <TokenContext>
@@ -12,7 +12,10 @@ createRoot(document.getElementById('root')).render(
           <App />   
         </div>
         <div className='message_wrapper'>
-           <h1 className='text-[20px] font-bold text-white'><span className='text-red-600'>!</span>Ekran sig'imi 1110px-dan kichik</h1>
+        <div className='fixed inset-0   justify-center top-56 items-center w-full z-0'>
+                 <img className="logo_animated mx-auto opacity-15" src={Logo} alt="Logo" width={200} height={200} />
+              </div>
+           <h1 className='text-[20px] font-bold text-white z-50'><span className='text-red-600'>!</span>Ekran sig'imi 1110px-dan kichik</h1>
         </div>
       </TokenContext>
     </BrowserRouter>
